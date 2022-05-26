b58f25fa7b8fb1d09dc7e7b325730c7f10c0876f
Darkknight1216 <102667090+Darkknight1216@users.noreply.github.com>
2022-05-26 16:02:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,12 +19,12 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['94751606250']
+global.premium = ['94751606250']
+global.ownernomer = '94751606250'
+global.ownername = '🐦Dark knight'
+global.botname = 'Dark Knight'
+global.footer = '©Dark knight.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
